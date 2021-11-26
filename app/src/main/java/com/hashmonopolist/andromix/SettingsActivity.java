e209d46ca7c4dc0e313d99b08d3c3e954adc44cc
superuser <hash@skyn3t.in>
2021-11-26 14:14:37 -0800

    add back button to SettingsActivity

--- app/src/main/java/com/hashmonopolist/andromix/SettingsActivity.java
+++ app/src/main/java/com/hashmonopolist/andromix/SettingsActivity.java
@@ -6,6 +6,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
+import android.view.MenuItem;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,7 +22,17 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        getSupportActionBar().setTitle("");
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_24);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        this.finish();
+        return true;
+    }
+
 
     @Override
     public void onBackPressed() {
